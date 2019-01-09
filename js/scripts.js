fbc39c491cfb8d208ef1345f78c22f2b08ec72e5
HipsterGrandpa <conlin.kyle@gmail.com>
2019-01-09 09:22:26 -0800

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $("form#insurance").submit(function(event){
+    var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
+
+    var quote = (100 - age) * 3;
+    if (gender === 'male' && age < 26){
+      quote += 50;
+    }
+
+    $("#rate").empty().append(quote);
+    $("#quote").show();
+  } else {
+    alert('Please enter your age.');
+  }
+
+    event.preventDefault();
+  });
+});
